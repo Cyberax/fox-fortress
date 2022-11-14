7ab13af76548e1fc4ff54c9d8d5201baabd56a81
Aleksei Besogonov <alex.besogonov@gmail.com>
2022-11-14 07:51:10 -0800

    Initial version of the plugin
    Allow opening Fortress tabs in separate containers

--- fortressify.js
+++ fortressify.js
@@ -1,2 +1,12 @@
-document.body.style.border = "5px solid red";
+// This is the low-security side of the extension that works in the context of the webpage.
+// It doesn't have full access to the browser's API
 
+// Get the settings for the target account
+const name = document.getElementById('account-name').innerText;
+const role = document.getElementById('role-name').innerText;
+const url = document.querySelector('a').href;
+
+// Send the settings to the high-security side of the extension
+browser.runtime.sendMessage({"url": url, "name": name, "role": role});
+
+//window.close();
---
+++ opener.js
@@ -0,0 +1,60 @@
+browser.runtime.onMessage.addListener(notify);
+
+async function notify(message, sender) {
+	var acctName = message.name;
+	var roleName = message.role;
+	var radioactive = true;
+	if (roleName === "Administrator") {
+		roleName = "adm";
+	}
+	if (roleName === "ReadOnly") {
+		roleName = "ro";
+		radioactive = false;
+	}
+	const isProd = acctName.endsWith("-prod") || acctName === "master";
+
+	const containerName = roleName + ":" + acctName;
+
+	const identity = await browser.contextualIdentities.query({"name": containerName});
+	var store;
+	if (identity.length == 0) {
+		var color = "yellow";
+		var icon = "briefcase";
+		if (isProd) {
+			icon = "dollar";
+			if (radioactive) {
+				color = "red";				
+			} else {
+				color = "orange";
+			}
+		} else {
+			if (radioactive) {
+				color = "yellow";
+			} else {
+				color = "green";
+			}
+		}
+
+		store = await browser.contextualIdentities.create({
+ 			name: containerName,
+			color: color,
+			icon: icon
+		});
+	} else {
+		const cookieStoreId = identity[0].cookieStoreId;
+		store = await browser.contextualIdentities.get(cookieStoreId);
+	}
+
+	const curTab = sender.tab;
+
+	let newTab = await browser.tabs.create({
+		active: false,
+		cookieStoreId: store.cookieStoreId,
+		url: message.url,
+		index: curTab.index
+	})
+
+	await browser.tabs.remove(curTab.id); // Remove the opening tab
+}
+
+
